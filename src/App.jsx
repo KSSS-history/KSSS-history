42873cd5682486dbc432afc44bbe4980c2cbb9b9
Marianna <maryanarez@gmail.com>
2024-11-21 13:12:58 +0100

    Style and routs adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import EventPage from "./pages/EventPage";
 import NoPage from "./pages/NoPage";
@@ -11,13 +11,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="event/:id" element={<EventPage />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="event/:id" element={<EventPage />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/container/main/events_provider/EventsProvider.jsx
+++ src/container/main/events_provider/EventsProvider.jsx
@@ -9,7 +9,11 @@ const EventsProvider = () => {
   const { getData } = useContentful();
 
   useEffect(() => {
-    getData().then((response) => setEvents(response));
+    getData()
+      .then((response) => {
+        setEvents(response);
+      })
+      .catch((error) => confsole.error("Error retrievering data", error));
   }, []);
 
   return (
@@ -18,28 +22,6 @@ const EventsProvider = () => {
         <EventCard key={id} event={event} />
       ))}
     </div>
-
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    //       or do this instead send data as props to EventCard.jsx
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    // <section className={styles.section}>
-    //   {events.map((event, id) => (
-    //     <div key={id} className={styles.section_content}>
-    //       <div>{event.fullYear}</div>
-    //       <h2>{event.headingSv}</h2>
-
-    //       <h3>{event.introTextSv}</h3>
-    //       <button>read more</button>
-    //       {event.eventImage?.file?.url && (
-    //         <img
-    //           className={styles.section_image}
-    //           src={event.eventImage.file.url}
-    //           alt={event.eventImage.title || "The Event image"}
-    //         />
-    //       )}
-    //     </div>
-    //   ))}
-    // </section>
   );
 };
 
