ef70302dde470071250178d3724be24e6afdebb8
Marianna <maryanarez@gmail.com>
2025-01-11 13:56:01 +0100

    1. useContentful fixxed to fetch next entries efter 100 limit.
    2. Header in position sticky.

--- src/utiles/datalayer/useContentful.js
+++ src/utiles/datalayer/useContentful.js
@@ -3,23 +3,42 @@ import client from "./client"
 const useContentful = () => {
 
 
-
     //function to fetch all events from "KSSS Event" content type:
     const getData = async () => {
-        try {
-            const entries = await client.getEntries({
-                content_type: "ksssEvent",
-                select: "fields",
-                order: "fields.fullYear" && "fields.date",
-                query: "" //here can go filtering
 
-            });
+        //Because there is a limit of 100 entries for every API call from Contentful, 
+        //we need to fetch the next 100 and so on until all entries are fetched.
+        try {
+            const limit = 100;
+            let skip = 0;
+            let total = 0;
+            let allEntries = [];
+
+            do {
+                const response = await client.getEntries({
+                    content_type: "ksssEvent",
+                    select: "fields",
+                    order: "fields.fullYear" && "fields.date",
+                    skip: skip,
+                    limit: limit,
+                    query: "" //here can go filtering
+
+                });
+
+                // Combine fetched entries into the main array
+                allEntries = [...allEntries, ...response.items];
+
+                // Update skip and total for the next iteration
+                skip += limit;
+                total = response.total;
+            } while (skip < total);
+
 
 
             //- - - - - - - - - - - - - - - - - - - - - - - - - - 
             //Sanitizing data from Contentful to get only fields that needed:
             //- - - - - - - - - - - - - - - - - - - - - - - - - - 
-            const sanitizedEntries = entries.items.map((item) => {
+            const sanitizedEntries = allEntries.map((item) => {
                 const introImage = item.fields.introImage?.fields || null;
                 const eventImages = item.fields.images?.map(img => img.fields) || null;
 
