b355fedccd0a2907e6dfda79e3cc7ad9deea44cf
Marianna <maryanarez@gmail.com>
2025-01-13 10:18:40 +0100

    Local Storage - done.

--- src/utiles/contexts/LanguageProvider.jsx
+++ src/utiles/contexts/LanguageProvider.jsx
@@ -1,14 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 //Create the context for the context object
 export const LanguageContext = createContext();
 
+//Initialise the language choice with a follow saving to the Local Storage
+const initialLanguage = JSON.parse(window.localStorage.getItem("language"));
+
 const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState("swedish");
+  const [language, setLanguage] = useState(initialLanguage);
 
   function changeLanguage(e) {
     setLanguage(e.target.value);
   }
+  //Safe the language in the Local Storage
+  useEffect(() => {
+    window.localStorage.setItem("language", JSON.stringify(language));
+  }, [language]);
 
   return (
     <LanguageContext.Provider value={{ language, changeLanguage }}>
